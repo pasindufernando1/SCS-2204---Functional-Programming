f4e38367ecaec6db855f4d5adbb7818c83777885
Pasindu Fernando <sulakshana1234567@gmail.com>
2022-08-22 23:45:42 +0530

    Update Question5.scala

--- Tutorial 5/Question5.scala
+++ Tutorial 5/Question5.scala
@@ -3,16 +3,16 @@ import scala.io.StdIn.readInt
 
 object Q5 extends App{
     
-    //Function to get the addition of all even numbers with regard to a given n
+    //Function to get the addition of all even numbers less than n
     def addEven(n:Int) : Int = n match {
-        case 0 => 0
-        case x if (x%2==0) => n + addEven(n-1)
-        case x => addEven(n-1)
+        case n if n<=2 => 0
+        case n if (n-1)%2==0 => (n-1) + addEven(n-1)
+        case n => addEven(n-1)
     } 
 
     //Printing sample output
     printf("Enter the value n : ")
     var n = readInt()
-    printf("The sum of the even numbers less than %d is : %d",n,addEven(n-1))
+    printf("The sum of the even numbers less than %d is : %d",n,addEven(n))
 }
 
